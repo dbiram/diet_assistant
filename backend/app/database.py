91f2768ade919d9541f5c11f656c31b994c4e006
dbiram <dbiramoez@hotmail.fr>
2025-08-07 19:33:11 +0200

    persistent database

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,10 +2,8 @@ from sqlalchemy import Column, Integer, String, Float, DateTime, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
-import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "../diet.db")
+DB_PATH =  "/var/data/diet.db"
 
 engine = create_engine(f"sqlite:///{DB_PATH}", connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
